c05f0f84f51ddc061080ee36f054af3f5d402cf7
Luzaks <kanemekanik@gmail.com>
2020-01-23 15:44:26 -0600

    Create the Sessions controller and it's views, complete the cycle with login methods and current user for the sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'users#new'
   get '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users, :only => [:new, :create, :show]
+  resources :sessions
 end
